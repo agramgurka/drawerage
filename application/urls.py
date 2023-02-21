b6d26fe3e94a88b24bffd8abc9803e1521b9405f
agram <agramgurka@gmail.com>
2023-02-21 22:12:19 +0300

    Fix redirects

--- application/urls.py
+++ application/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import StartPage, CreateGame, Game, Proxy, MediaUpload
+from .views import StartPage, CreateGame, Game, MediaUpload
 
 urlpatterns = [
-    path('', Proxy.as_view(), name='proxy'),
-    path('start/', StartPage.as_view(), name='start_page'),
+    path('', StartPage.as_view(), name='start_page'),
     path('create/', CreateGame.as_view(), name='create_game'),
     path('game/<int:pk>', Game.as_view(), name='game'),
     path('upload/', MediaUpload.as_view(), name='media_upload')
