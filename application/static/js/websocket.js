3247d810545298359ee9b8bd87a9340398e62767
agram <agramgurka@gmail.com>
2023-03-06 16:53:19 +0300

    make client's websocket reconnect if connection with server is lost

--- application/static/js/websocket.js
+++ application/static/js/websocket.js
@@ -1,58 +1,77 @@
+var ws;
+var reconnectionCnt = 0;
+const reconnectionDelay = 1000;
+const maxReconnectionCnt = 5;
+
 function connect() {
-    return new WebSocket(
+    ws = new WebSocket(
       (window.location.protocol === 'https:' ? 'wss://' : 'ws://') +
       window.location.host +
       '/ws/game/' +
       gameId +
       '/'
     );
+    ws.onopen = function () {
+        reconnectionCnt = 0;
+        ws.send(
+            JSON.stringify({
+                "command": "connected",
+            })
+        );
+    };
+    ws.onmessage = function(response) {
+        const res = JSON.parse(response.data);
+        const command = res.command;
+        if (command == "update") {
+            const activeScreen = res.active_screen;
+            let screen = null;
+            if (activeScreen == "status") {
+                screen = initStatusScreen(res.players, res.task_type)
+            }
+            if (activeScreen == "task") {
+                screen = initTaskScreen(res.task_type, res.task);
+            }
+            if (activeScreen == "results") {
+                screen = initResultsScreen(res.results);
+            }
+            if (activeScreen == "answers") {
+                screen = initAnswersScreen(res.variants);
+            }
+            if (activeScreen == "final_standings") {
+                screen = initFinalStandingsScreen(res.results);
+            }
+            displayScreen(screen);
+        }
+        if (command == "display_answer") {
+            displayAnswer(res.variant, res.is_correct);
+        }
+        if (command == "pause") {
+            displayPausePopup(res.text);
+        }
+        if (command == "resume") {
+            hidePausePopup();
+        }
+        if (command == "init_stage") {
+            initStage(res.stage);
+        }
+        if (command == "timer") {
+            setTimer(res.initial, res.left);
+        }
+    };
+    ws.onerror = function(err) {
+        ws.close();
+    };
+    ws.onclose = function () {
+        reconnectionCnt++;
+        if (reconnectionCnt <= maxReconnectionCnt)
+            setTimeout(function () {
+                displayPausePopup("Reconnecting to server");
+                connect();
+            }, reconnectionDelay);
+        else {
+            displayPausePopup('Server is unavailable');
+        }
+    };
 }
 
-const ws = connect();
-
-ws.onopen = function () {
-    ws.send(
-        JSON.stringify({
-            "command": "connected",
-        })
-    )};
-
-ws.onmessage = function(response) {
-    const res = JSON.parse(response.data);
-    const command = res.command;
-    if (command == "update") {
-        const activeScreen = res.active_screen;
-        let screen = null;
-        if (activeScreen == "status") {
-            screen = initStatusScreen(res.players, res.task_type)
-        }
-        if (activeScreen == "task") {
-            screen = initTaskScreen(res.task_type, res.task);
-        }
-        if (activeScreen == "results") {
-            screen = initResultsScreen(res.results);
-        }
-        if (activeScreen == "answers") {
-            screen = initAnswersScreen(res.variants);
-        }
-        if (activeScreen == "final_standings") {
-            screen = initFinalStandingsScreen(res.results);
-        }
-        displayScreen(screen);
-    }
-    if (command == "display_answer") {
-        displayAnswer(res.variant, res.is_correct);
-    }
-    if (command == "pause") {
-        displayPausePopup(res.text);
-    }
-    if (command == "resume") {
-        hidePausePopup();
-    }
-    if (command == "init_stage") {
-        initStage(res.stage);
-    }
-    if (command == "timer") {
-        setTimer(res.initial, res.left);
-    }
-};
+connect();
