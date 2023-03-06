9292dd94d70100a3493275799034e599bbbe7975
Ivan Belokobylskiy <belokobylskij@gmail.com>
2023-03-06 11:58:37 +0530

    Fix static serving with django

--- Drawesome/urls.py
+++ Drawesome/urls.py
@@ -1,10 +1,26 @@
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+import re
 
-from . import settings
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('application.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.SERVE_STATIC_WITH_DJANGO:
+    urlpatterns += [
+        re_path(
+            r"^%s(?P<path>.*)$" % re.escape(settings.MEDIA_URL.lstrip("/")),
+            serve,
+            kwargs=dict(document_root=settings.MEDIA_ROOT)
+        ),
+    ]
+    urlpatterns += [
+        re_path(
+            r"^%s(?P<path>.*)$" % re.escape(settings.STATIC_URL.lstrip("/")),
+            serve,
+            kwargs=dict(document_root=settings.STATIC_ROOT)
+        ),
+    ]
