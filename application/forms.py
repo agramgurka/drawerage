fa84a5833b91b0a218480700fe5fc485782efbeb
agram <agramgurka@gmail.com>
2023-03-11 01:58:22 +0300

    restrict username and game code length

--- application/forms.py
+++ application/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from .services.basics import GAME_CODE_LEN, NICKNAME_LEN
 
 
 class JoinGameForm(forms.Form):
-    game_code = forms.CharField(max_length=10)
-    player_nickname = forms.CharField(max_length=100)
+    game_code = forms.CharField(max_length=GAME_CODE_LEN)
+    player_nickname = forms.CharField(max_length=NICKNAME_LEN)
 
     game_code.widget.attrs.update({'class': 'form-control'})
     player_nickname.widget.attrs.update({'class': 'form-control'})
