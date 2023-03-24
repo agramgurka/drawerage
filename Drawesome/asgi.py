81b204aa0fde495598492abda400d82d602149ed
Ivan Belokobylskiy <belokobylskij@gmail.com>
2023-03-24 11:07:19 +0530

    Fix asgi entrypoint

--- Drawesome/asgi.py
+++ Drawesome/asgi.py
@@ -5,11 +5,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-import application.routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Drawesome.settings')
-
 django_asgi_app = get_asgi_application()
+
+import application.routing  # noqa: E402
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
